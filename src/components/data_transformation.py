42c11c4292dfc146549b50abe810d7e644c787a3
Lakshay Goel <iamlakshaygoel5990@gmail.com>
2025-05-22 23:03:15 +0530

    large file

---
+++ src/components/data_transformation.py
@@ -0,0 +1,114 @@
+import sys
+import pandas as pd
+import numpy as np
+
+from imblearn.over_sampling import RandomOverSampler
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+
+from src.constants import TARGET_COLUMN, SCHEMA_FILE_PATH
+from src.entity.config_entity import DataTransformationConfig
+from src.entity.artifact_entity import DataTransformationArtifact, DataIngestionArtifact, DataValidationArtifact
+from src.exception import USvisaException
+from src.logger import logging
+from src.utils.main_utils import save_object, save_numpy_array_data, read_yaml_file, drop_columns
+from src.entity.estimator import TargetValueMapping
+
+
+class DataTransformation:
+    def __init__(self, data_ingestion_artifact: DataIngestionArtifact,
+                 data_transformation_config: DataTransformationConfig,
+                 data_validation_artifact: DataValidationArtifact):
+        try:
+            self.data_ingestion_artifact = data_ingestion_artifact
+            self.data_transformation_config = data_transformation_config
+            self.data_validation_artifact = data_validation_artifact
+            self._schema_config = read_yaml_file(file_path=SCHEMA_FILE_PATH)
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    @staticmethod
+    def read_data(file_path) -> pd.DataFrame:
+        try:
+            return pd.read_csv(file_path)
+        except Exception as e:
+            raise USvisaException(e, sys)
+
+    def get_data_transformer_object(self) -> ColumnTransformer:
+        logging.info("Entered get_data_transformer_object method of DataTransformation class")
+
+        try:
+            numeric_transformer = StandardScaler(with_mean=True)
+            oh_transformer = OneHotEncoder()
+
+            oh_columns = self._schema_config['ohe_encoder']
+            num_features =self._schema_config['standard_scaler']
+
+            preprocessor = ColumnTransformer(
+                [
+                    ("OneHotEncoder", oh_transformer, oh_columns),
+                    ("StandardScaler", numeric_transformer, num_features)
+                ],
+                sparse_threshold=0  # 🔥 Force output to be dense (needed for StandardScaler with mean)
+            )
+
+            logging.info("Created preprocessor object from ColumnTransformer")
+            logging.info("Exited get_data_transformer_object method of DataTransformation class")
+            return preprocessor
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
+
+    def initiate_data_transformation(self) -> DataTransformationArtifact:
+        try:
+            if self.data_validation_artifact.validation_status:
+                logging.info("Starting data transformation")
+                preprocessor = self.get_data_transformer_object()
+                logging.info("Got the preprocessor object")
+
+                train_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.trained_file_path)
+                test_df = DataTransformation.read_data(file_path=self.data_ingestion_artifact.test_file_path)
+
+                drop_cols = self._schema_config['drop_columns']
+                train_df = drop_columns(df=train_df, cols=drop_cols)
+                train_df.dropna(inplace=True)
+
+                input_feature_train_df = train_df.drop(columns=[TARGET_COLUMN], axis=1)
+                target_feature_train_df = train_df[TARGET_COLUMN].replace(TargetValueMapping()._asdict())
+
+                test_df = drop_columns(df=test_df, cols=drop_cols)
+                test_df.dropna(inplace=True)
+
+                input_feature_test_df = test_df.drop(columns=[TARGET_COLUMN], axis=1)
+                target_feature_test_df = test_df[TARGET_COLUMN].replace(TargetValueMapping()._asdict())
+
+                logging.info("Applying preprocessing object on training dataframe and testing dataframe")
+                input_feature_train_arr = preprocessor.fit_transform(input_feature_train_df)
+                input_feature_test_arr = preprocessor.transform(input_feature_test_df)
+
+                smt = RandomOverSampler()
+                input_feature_train_final, target_feature_train_final = smt.fit_resample(
+                    input_feature_train_arr, target_feature_train_df)
+                input_feature_test_final, target_feature_test_final = smt.fit_resample(
+                    input_feature_test_arr, target_feature_test_df)
+
+                train_arr = np.c_[input_feature_train_final, np.array(target_feature_train_final)]
+                test_arr = np.c_[input_feature_test_final, np.array(target_feature_test_final)]
+
+                save_object(self.data_transformation_config.transformed_object_file_path, preprocessor)
+                save_numpy_array_data(self.data_transformation_config.transformed_train_file_path, array=train_arr)
+                save_numpy_array_data(self.data_transformation_config.transformed_test_file_path, array=test_arr)
+
+                logging.info("Saved the preprocessor object and transformed arrays")
+
+                return DataTransformationArtifact(
+                    transformed_object_file_path=self.data_transformation_config.transformed_object_file_path,
+                    transformed_train_file_path=self.data_transformation_config.transformed_train_file_path,
+                    transformed_test_file_path=self.data_transformation_config.transformed_test_file_path
+                )
+            else:
+                raise Exception(self.data_validation_artifact.message)
+
+        except Exception as e:
+            raise USvisaException(e, sys) from e
--- src/constants/__init__.py
+++ src/constants/__init__.py
@@ -18,7 +18,7 @@ FILE_NAME: str = "usvisa.csv"
 MODEL_FILE_NAME = "model.pkl"
 
 
-TARGET_COLUMN = "case_status"
+TARGET_COLUMN = "RainTomorrow"
 CURRENT_YEAR = date.today().year
 PREPROCSSING_OBJECT_FILE_NAME = "preprocessing.pkl"
 SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
